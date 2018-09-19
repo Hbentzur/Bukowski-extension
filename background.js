04b5d62e1101b151ee8900d42fdb31ce10aa67a6
Hbentzur <31830472+Hbentzur@users.noreply.github.com>
2018-09-18 23:42:41 -0400

    Add files via upload

---
+++ background.js
@@ -0,0 +1,32 @@
+var webcamHtml = chrome.extension.getURL('webcam.html');
+var cam;
+var popupid;
+var tab;
+let Mouthopen = true;
+
+console.log('Bukowski!');
+
+// Extantion Hello world
+chrome.tabs.onCreated.addListener(function(tab) {
+  console.log('new tab');
+});
+
+// Open camera
+chrome.browserAction.onClicked.addListener(function(tab) {
+  console.log('camera is here!');
+  cam = window.open(webcamHtml, '_blank', "height=250,width=340");
+  chrome.windows.getCurrent(function(win) {
+    console.log(win.id);
+    win.id = popupid;
+  });
+});
+
+
+// If the mouth is open (based on the distance between the lips), open new tab
+chrome.runtime.onMessage.addListener(function(message, tab) {
+  console.log(message);
+  if (message > 15) {
+    window.open('https://hbentzur.github.io/Bukowski/', '_blank');
+    cam.focus();
+  }
+});
